5c227caf128345b85de33e092bef5982ddc50d5b
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2018-05-06 19:58:56 -0500

    styles: add mat-typography & move header to layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderModule } from '@shared/header/index.module';
+import { MatButtonModule } from '@angular/material';
+
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    MatButtonModule
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
--- src/app/landing/index.module.ts
+++ src/app/landing/index.module.ts
@@ -1,29 +1,25 @@
-import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-import { CommonAppModule } from '@shared/common';
-import { HeaderModule } from '@shared/header/index.module';
-import { LandingComponent } from './components/container/index.component';
-
 import { MatButtonModule } from '@angular/material';
 
-
-
+import { CommonAppModule } from '@shared/common';
+import { LandingComponent } from './components/container/index.component';
 
 const routes: Routes = [
     { path: "", component: LandingComponent }
 ]
 
 @NgModule({
-    imports: [RouterModule.forChild(routes),
+    imports: [
+        RouterModule.forChild(routes),
         CommonModule,
-        CommonAppModule,HeaderModule,
-        MatButtonModule],
+        CommonAppModule,
+        MatButtonModule
+    ],
     exports: [],
     declarations: [LandingComponent],
-    providers: [],
-    schemas:[ NO_ERRORS_SCHEMA ]
+    providers: []
 })
 export class LandingModule { }
 
