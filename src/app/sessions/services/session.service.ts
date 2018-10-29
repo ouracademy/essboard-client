f81a7bc28f05e7b23b2ae88203ebdef47cd7f052
qpdiam <qpdiam@gmail.com>
2018-10-28 22:14:09 -0500

    feat: add getSessions by projectId to abstract class

--- src/app/sessions/services/session.service.ts
+++ src/app/sessions/services/session.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs'
 export abstract class SessionService {
   currentSession$: Subject<any>
   abstract getSession(id: string)
+  abstract getSessions(projectId: string)
   abstract addSession(projectId: string, lastSessionId: string)
   abstract delete(id)
   abstract setCheckpointTo(id, dimensionId, stateId, checkpointId, condition)
