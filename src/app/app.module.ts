beca73431899bc8e4fc6686dffb665945659fc9f
qpdiam <qpdiam@gmail.com>
2018-10-29 00:00:18 -0500

    feat: add es locale for date pipes :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core'
+import { NgModule, LOCALE_ID } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { CommonModule } from '@angular/common'
+import { CommonModule, registerLocaleData } from '@angular/common'
 
 import { CoreModule } from './core-module/index.module'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+
+import localeEs from '@angular/common/locales/es'
+registerLocaleData(localeEs)
 
 @NgModule({
   imports: [
@@ -15,6 +18,7 @@ import { AppComponent } from './app.component'
     AppRoutingModule
   ],
   declarations: [AppComponent],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
