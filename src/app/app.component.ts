d399963d1eb59ba55720f4f1094f86674e4503ae
qpdiam <qpdiam@gmail.com>
2018-10-29 21:41:38 -0500

    fix: first solution for Not auth error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AuthService } from '@core/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { AuthService } from '@core/auth.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent  {
-  constructor( private auth: AuthService ) { 
-  
+export class AppComponent {
+  constructor(private auth: AuthService) {
+    this.auth.reconnect()
   }
-
 }
--- src/app/core-module/auth.service.ts
+++ src/app/core-module/auth.service.ts
@@ -1,92 +1,95 @@
-import { Injectable } from '@angular/core';
-import { SocketService } from '@core/socket.service';
-import { Credentials, User } from '@no-module/models/user';
-import { tap } from 'rxjs/operators';
-import { BehaviorSubject, Subject, Observable } from 'rxjs';
-import { NotificationService } from './notification.service';
-import { KernelService } from './kernel-knowledge.service';
+import { Injectable } from '@angular/core'
+import { SocketService } from '@core/socket.service'
+import { Credentials, User } from '@no-module/models/user'
+import { tap } from 'rxjs/operators'
+import { BehaviorSubject, Subject, Observable } from 'rxjs'
+import { NotificationService } from './notification.service'
+import { KernelService } from './kernel-knowledge.service'
 
 @Injectable()
 export class AuthService {
-    // store the URL so we can redirect after logging in
-    private _redirectURL: string;
+  // store the URL so we can redirect after logging in
+  private _redirectURL: string
 
+  constructor(
+    public socketService: SocketService,
+    private notification: NotificationService
+  ) {}
 
-    constructor(
-        public socketService: SocketService,
-        private notification: NotificationService) {
+  set redirectURL(URL: string) {
+    this._redirectURL = URL
+  }
 
+  get redirectURL() {
+    return this._redirectURL ? this._redirectURL : '/me/projects'
+  }
+
+  public login(credentials: Credentials): Promise<User> {
+    return this.socketService
+      .authenticate({
+        strategy: 'local',
+        email: credentials.email,
+        password: credentials.password
+      })
+      .then(result => {
+        window.localStorage.setItem('user', JSON.stringify(result))
+        this.notification.startSubscription()
+        return this.user
+      })
+  }
+
+  public reconnect() {
+    const token = window.localStorage.getItem('feathers-jwt')
+    if (token) {
+      this.socketService
+        .logout()
+        .then(() => {
+          this.socketService.authenticate({ type: 'token', token: token })
+        })
+        .catch(err => console.log('err', err))
     }
+  }
 
-    set redirectURL(URL: string) {
-        this._redirectURL = URL;
-    }
+  public get isLoggedIn(): boolean {
+    return !!window.localStorage.getItem('user')
+  }
 
-    get redirectURL() {
-        return this._redirectURL ? this._redirectURL : '/me/projects';
-    }
+  public logout(): Promise<boolean> {
+    return this.socketService.logout().then(() => {
+      localStorage.removeItem('user')
+      return true
+    })
+  }
 
-    public login(credentials: Credentials): Promise<User> {
-        return this.socketService.authenticate({
-            'strategy': 'local',
-            'email': credentials.email,
-            'password': credentials.password
-        }).then((result) => {
-            window.localStorage.setItem('user', JSON.stringify(result));
-            this.notification.startSubscription()
-            return this.user;
-        }).catch(function (error) {
-            console.log(error)
-            throw error;
-        });
+  public get user(): User {
+    const data = JSON.parse(window.localStorage.getItem('user'))
+    return data
+      ? new User(
+          data['_id'],
+          data['name'],
+          data['email'],
+          data['createdAt'],
+          data['appKeyTrello']
+        )
+      : new User('', '')
+  }
+  public set user(user: User) {
+    window.localStorage['user'] = JSON.stringify(user)
+  }
+  public haveThisSession(sessionId: string): boolean {
+    const data = JSON.parse(window.localStorage.getItem('user'))
+    return data.sessionsId.indexOf(sessionId) !== -1
+  }
 
-    }
-
-    public loginWithToken() {
-        // const token = window.localStorage.getItem('feathers-jwt')
-        // if( token ){
-        //     this.logoutToLogin().then( () =>{
-        //         this.socketService.authenticate({type: 'token', token: token})
-        //     })
-        // }
-    }
-
-
-    public get isLoggedIn(): boolean {
-        return !!window.localStorage.getItem('user');
-    }
-
-    public logout(): Promise<boolean> {
-        return this.socketService.logout().then(() => {
-            localStorage.removeItem('user');
-            return true;
-        });
-    }
-
-    private logoutToLogin(): Promise<boolean> {
-        return this.socketService.logout().then(() => {
-            return true;
-        });
-    }
-
-    public get user(): User {
-        const data = JSON.parse(window.localStorage.getItem('user'));
-        return data ? new User(data['_id'], data['name'], data['email'], data['createdAt'], data['appKeyTrello']) : new User('', '');
-    }
-    public set user(user: User) {
-        window.localStorage['user'] = JSON.stringify(user);
-    }
-    public haveThisSession(sessionId: string): boolean {
-        const data = JSON.parse(window.localStorage.getItem('user'));
-        return data.sessionsId.indexOf(sessionId) !== -1;
-    }
-
-    public signup(user: User) {
-        return this.socketService.getService('users').watch().create({
-            name: user.name,
-            email: user.email,
-            password: user.password
-        }).pipe(
-            tap(val => this.login(new Credentials(user.email, user.password))))
-    }
+  public signup(user: User) {
+    return this.socketService
+      .getService('users')
+      .watch()
+      .create({
+        name: user.name,
+        email: user.email,
+        password: user.password
+      })
+      .pipe(tap(val => this.login(new Credentials(user.email, user.password))))
+  }
 }
