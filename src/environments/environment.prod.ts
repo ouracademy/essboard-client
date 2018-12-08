9605a95ccdce03285de57ea87deb8d7c774ccb26
qpdian <qpdiam@gmail.com>
2018-12-07 22:21:57 -0500

    fix: url core and kernel api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true
 }
 
-
-export const coreApi = 'https://essboard-core-api.herokuapp.com/' 
-export const kernelApi = 'https://essboard-kernel-api.herokuapp.com/'
+export const coreApi = 'https://essboard-core-api.herokuapp.com'
+export const kernelApi = 'https://essboard-kernel-api.herokuapp.com'
