ebb0992df73561dc3912137f01916b636e6cb5de
artmadeit <this.arthur.email@gmail.com>
2018-12-08 19:40:43 -0500

    style: make avatar gray when offline

--- src/app/shared/avatar/index.component.ts
+++ src/app/shared/avatar/index.component.ts
@@ -3,32 +3,24 @@ import { randomColor } from '@shared/utils/random-color'
 @Component({
   selector: 'app-our-avatar',
   template: `
-    <ngx-avatar
-      [size]="size"
-      [name]="name"
-      [bgColor]="color"
-      [class.offline]="!isOnline"
-    ></ngx-avatar>
-  `,
-  styles: [
-    `
-      .offline {
-        opacity: 0.25;
-      }
-    `
-  ]
+    <ngx-avatar [size]="size" [name]="name" [bgColor]="color"></ngx-avatar>
+  `
 })
 export class OurAvatarComponent implements OnInit {
   @Input('user')
   set user(data) {
-    this.color = randomColor(data.name, 22)
     this.name = data.name
   }
+
   @Input() size = 30
   @Input() isOnline
-  color = ''
+
   name = ''
   constructor() {}
 
   ngOnInit() {}
+
+  get color() {
+    return this.isOnline ? randomColor(this.name, 22) : '#80808030'
+  }
 }
