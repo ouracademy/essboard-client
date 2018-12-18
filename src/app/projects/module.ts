137435cf7304e1b354de02b4b5d353ce89822647
qpdiam <qpdiam@gmail.com>
2018-12-18 13:00:05 -0500

    feat: alpha card, state and cheklist responsive

--- src/app/projects/module.ts
+++ src/app/projects/module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import {
   MatCardModule,
-  MatProgressBarModule,
   MatButtonModule,
-  MatToolbarModule,
   MatFormFieldModule,
   MatInputModule,
-  MatRadioModule,
   MatIconModule,
-  MatDialogModule
+  MatDialogModule,
+  MatTooltipModule
 } from '@angular/material'
-
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
+import { RoutingModule } from './routing.module'
 import { KernelModule } from '@shared/kernel/kernel.module'
 import { LoadingModule } from '@shared/loading/loading.module'
 import { SearchUsersModule } from '../users/search.module'
-
-import { RoutingModule } from './routing.module'
+import { RenderCtrlModule } from '@shared/render-ctrl/index.module'
+import { NotImplementedModule } from '@shared/not-implemented/index.module'
+import { OurAvatarModule } from '@shared/avatar/index.module'
+import { LoadingWhenClickedModule } from '@shared/loading-when-clicked'
 
 import {
   ListComponent,
@@ -28,35 +28,30 @@ import {
   CreateComponent,
   ShareComponent
 } from './index'
-import { RenderCtrlModule } from '@shared/render-ctrl/index.module'
-import { NotImplementedModule } from '@shared/not-implemented/index.module'
-import { OurAvatarModule } from '@shared/avatar/index.module'
-import { LoadingWhenClickedModule } from '@shared/loading-when-clicked'
+
 @NgModule({
   imports: [
     CommonModule,
-    LoadingWhenClickedModule,
     ReactiveFormsModule,
-    RenderCtrlModule,
-    NotImplementedModule,
+    FormsModule,
 
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatRadioModule,
     MatIconModule,
-    MatToolbarModule,
-    MatProgressBarModule,
     MatDialogModule,
+    MatTooltipModule,
+
     SearchUsersModule,
-
     LoadingModule,
-    RoutingModule,
+    RenderCtrlModule,
+    NotImplementedModule,
+    LoadingWhenClickedModule,
 
-    FormsModule,
     KernelModule,
-    OurAvatarModule
+    OurAvatarModule,
+    RoutingModule
   ],
   declarations: [
     ListComponent,
--- src/app/shared/header/index.component.ts
+++ src/app/shared/header/index.component.ts
@@ -4,21 +4,28 @@ import { Component, OnInit, Input } from '@angular/core'
   selector: 'app-header',
   template: `
     <div
-      class="row start-xs middle-xs"
-      [ngStyle]="{ height: mode === 'toolbar' ? '15vh' : '20vh' }"
+      class="row between-xs middle-xs"
+      [ngStyle]="{ height: mode === 'toolbar' ? '10vh' : '20vh' }"
     >
-      <a class="brand col-xs-6 col-sm-6 " routerLink="/">
+      <a class="brand col-xs-4 col-sm-4 " routerLink="/">
         <img
           class="responsive-width"
           src="assets/images/logo/logo-horizontal.png"
           alt="Essboard"
         />
       </a>
-      <div class="p-3 row end-xs middle-xs col-xs-6 col-sm-6">
+      <div class="p-1 row end-xs middle-xs">
         <ng-content select="right-content"> </ng-content>
       </div>
     </div>
-  `
+  `,
+  styles: [
+    `
+      .brand {
+        height: 100%;
+      }
+    `
+  ]
 })
 export class HeaderComponent implements OnInit {
   @Input() mode
