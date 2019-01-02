2802d03b1ed38d1d79be01f293e75e7a404bba59
artmadeit <this.arthur.email@gmail.com>
2019-01-01 20:53:23 -0500

    refactor: remove env??

--- src/app/core-module/env.ts
+++
@@ -1,7 +0,0 @@
-export const env = {
-  api: {
-    second: 'https://essboard-backend-marthur.c9users.io/',
-    local: 'http://localhost:3030',
-    main: 'https://essboard-backend.herokuapp.com/'
-  }
-}
