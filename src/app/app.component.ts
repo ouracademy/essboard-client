af80d530fd680b44370079c4551f8fe50805d67d
qpdian <qpdiam@gmail.com>
2019-02-08 01:12:47 -0500

    feat: listen network status and show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core'
+import { Component, HostListener } from '@angular/core'
 import { AuthService } from '@core/auth.service'
+import { SharedService } from '@core/shared.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor(private auth: AuthService) {
-    this.auth.reconnect()
-  }
+  constructor(private auth: AuthService, public sharedService: SharedService) {}
+
+  //this.auth.reconnect()
 }
