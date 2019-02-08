2c12c318211c031663ca81d9f69aca0bc5d53b0a
qpdian <qpdiam@gmail.com>
2019-02-08 01:10:35 -0500

    feat: install and use  ngx offline in service

--- src/app/core-module/shared.service.ts
+++ src/app/core-module/shared.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core'
 import { NgxAlertsService } from '@ngx-plus/ngx-alerts'
+import { Network } from '@ngx-pwa/offline'
 
 @Injectable()
 export class SharedService {
-  constructor(private alertService: NgxAlertsService) {}
+  constructor(
+    private alertService: NgxAlertsService,
+    private network: Network
+  ) {}
 
   showError(title, text = '') {
     this.alertService.alertError({ title, text })
   }
   showSucces(title, text = '') {
     this.alertService.alertSuccess({ title, text })
+  }
+  get networkStatus() {
+    return this.network.onlineChanges
+  }
+  get isOnline() {
+    return this.network.online
   }
 }
