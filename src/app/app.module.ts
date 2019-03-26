132aad8e3548ccc8ddfb4b57268f47ffafba4236
artmadeit <this.arthur.email@gmail.com>
2019-03-26 16:51:39 -0500

    fix: bug in using hammer on material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,30 @@ import { AppComponent } from './app.component'
 
 import localeEs from '@angular/common/locales/es'
 import { LoadingClickService } from '@shared/loading-when-clicked'
+import { GestureConfig } from '@angular/material'
+import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser'
 
 registerLocaleData(localeEs)
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+
     CommonModule,
     CoreModule,
     AppRoutingModule
   ],
   declarations: [AppComponent],
-  providers: [{ provide: LOCALE_ID, useValue: 'es' }, LoadingClickService],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' },
+    LoadingClickService,
+    {
+      // See https://github.com/angular/material2/issues/4278#issuecomment-317832711
+
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: GestureConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
