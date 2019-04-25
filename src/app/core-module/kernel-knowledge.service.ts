7d9132ab33f6ebf0da0d585b74b49728edd813cd
qpdian <qpdiam@gmail.com>
2019-04-25 13:12:05 -0500

    feat: request kernel when initialized essboard

--- src/app/core-module/kernel-knowledge.service.ts
+++ src/app/core-module/kernel-knowledge.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
 import { kernelApi } from '@env/environment'
-import { CheckpointTemplate } from 'app/sessions/components/detail-alpha/kernel'
-import { AlphaTemplate } from 'app/sessions/components/detail-alpha/kernel'
+import {
+  AlphaTemplate,
+  CheckpointTemplate
+} from 'app/sessions/components/detail-alpha/kernel'
 import { Observable } from 'rxjs/Observable'
 
 @Injectable()
 export class KernelService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.requestToOpenServer()
+  }
 
   getAlphas(): Observable<AlphaTemplate[]> {
     return this.httpClient.get<any[]>(`${kernelApi}/kernel/alphas`)
@@ -22,6 +26,12 @@ export class KernelService {
   getCheckpoints(stateId): Observable<CheckpointTemplate[]> {
     return this.httpClient.get<CheckpointTemplate[]>(
       `${kernelApi}/kernel/states/${stateId}/checkpoints`
+    )
+  }
+
+  requestToOpenServer() {
+    return this.httpClient.get<CheckpointTemplate[]>(
+      `${kernelApi}/kernel/alphas`
     )
   }
 }
